a3f3e0bf4dad82f66bad1cbed3d22f61fe0e1377
vicbguti <vborborgutierrez@gmail.com>
2026-03-31 17:41:13 -0500

    avoid buffer overflow by image saving on setProjects

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       return false;
     }
 
-    setProjects((prev) => [newItem, ...prev]);
+    setProjects((prev) => [saved, ...prev]);
 
     navigate(`/visualizer/${newId}`, {
       state: {
