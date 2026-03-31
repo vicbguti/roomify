88aa6be5132dc4624b6507b6be7b5a74e527b010
vicbguti <vborborgutierrez@gmail.com>
2026-03-31 16:39:50 -0500

    host and upload images

--- components/lib/puter.action.ts
+++ components/lib/puter.action.ts
@@ -1,4 +1,7 @@
 import puter from "@heyputer/puter.js";
+import {getOrCreateHostingConfig, uploadImageToHosting} from "./putter.hosting";
+import upload from "../Upload";
+import {isHostedUrl} from "./utils";
 
 export const signIn = async () => await puter.auth.signIn();
 
@@ -8,6 +11,56 @@ export const getCurrentUser = async () => {
     try {
         return await puter.auth.getUser();
     } catch {
+        return null;
+    }
+}
+
+export const createProject = async ({ item }: CreateProjectParams): Promise<DesignItem | null | undefined> => {
+    const projectId = item.id;
+
+    const hosting = await getOrCreateHostingConfig();
+
+    const hostedSource = projectId ?
+        await uploadImageToHosting({ hosting, url: item.sourceImage, projectId, label: 'source', }): null;
+
+    const hostedRender = projectId && item.renderedImage ?
+        await uploadImageToHosting({ hosting, url: item.renderedImage, projectId, label: 'rendered', }): null;
+
+    const resolvedSource = hostedSource?.url || (isHostedUrl(item.sourceImage)
+        ? item.sourceImage
+        : ''
+    );
+
+    if (!resolvedSource) {
+        console.warn('Failed to host source image, skipping save.');
+        return null;
+    }
+
+    const resolvedRender = hostedRender?.url
+        ? hostedRender?.url
+        : item.renderedImage && isHostedUrl(item.renderedImage)
+            ? item.renderedImage
+            : undefined;
+
+    const {
+        sourcePath: _sourcePath,
+        renderedPath: _renderedPath,
+        publicPath: _publicPath,
+        ...rest
+    } = item;
+
+    const payload =  {
+        ...rest,
+        sourceImage: resolvedSource,
+        renderedImage: resolvedRender,
+    }
+    
+    try {
+        // Call the Puter worker to store project in kv
+
+        return payload;
+    } catch (e) {
+        console.log('Failed to save project', e)
         return null;
     }
 }
